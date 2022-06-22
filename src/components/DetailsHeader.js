f5a19af26f5565681e8abfe248d2708e49dd7e55
SimbaWANGU <simbawangulu@gmail.com>
2022-06-22 13:27:17 +0300

    Add detailsheader section in details page

--- src/components/DetailsHeader.js
+++ src/components/DetailsHeader.js
@@ -1,8 +1,14 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import '../styles/DetailsHeader.scss';
 
-function DetailsHeader() {
+function DetailsHeader({ name, photo, team }) {
   return (
-    <div>DetailsHeader</div>
+    <div className="detailsHeader">
+      <img src={photo} alt="Scorer" />
+      <h1>{name}</h1>
+      <p>{team}</p>
+    </div>
   );
 }
 
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,28 +1,56 @@
+/* eslint-disable quotes */
 /* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import DetailsBody from '../components/DetailsBody';
+import DetailsHeader from '../components/DetailsHeader';
 import Header from '../components/Header';
 import '../styles/Details.scss';
 
 function Details() {
-  const selector = useSelector((state) => (state.topScorer));
+  // const selector = useSelector((state) => (state.topScorer));
 
-  const getScorerData = () => {
-    const urlArray = window.location.href.split('/');
-    const arrayLength = urlArray.length - 1;
-    const scorer = selector.find((object) => object.id === parseInt(urlArray[arrayLength], 10));
-    return scorer;
+  // const getScorerData = () => {
+  //   const urlArray = window.location.href.split('/');
+  //   const arrayLength = urlArray.length - 1;
+  //   const scorer = selector.find((object) => object.id === parseInt(urlArray[arrayLength], 10));
+  //   return scorer;
+  // };
+
+  // useEffect(() => {}, [getScorerData()]);
+
+  // const scorerData = getScorerData();
+
+  const scorerData = {
+    appearances: 35,
+    goalsScored: 23,
+    id: 186,
+    minutesPlayed: 3021,
+    name: "Heung-Min Son",
+    photo: "https://media.api-sports.io/football/players/186.png",
+    rating: "7.505714",
+    shotsOnTarget: 49,
+    team: "Tottenham",
+    totalShots: 69,
   };
 
-  useEffect(() => {}, [getScorerData()]);
-
-  const scorerData = getScorerData();
-
   return (
-    <>
+    <div className="details">
       <Header />
-      <div>Details</div>
-    </>
+      <DetailsHeader
+        name={scorerData.name}
+        photo={scorerData.photo}
+        team={scorerData.team}
+      />
+      <DetailsBody
+        rating={scorerData.rating}
+        appearances={scorerData.appearances}
+        goalsScored={scorerData.goalsScored}
+        minutesPlayed={scorerData.minutesPlayed}
+        totalShots={scorerData.totalShots}
+        shotsOnTarget={scorerData.shotsOnTarget}
+      />
+    </div>
   );
 }
 
